75b58a28f9109af0b0b29baa4263550b8a8df570
Danilum <35626917+Danilum@users.noreply.github.com>
2018-08-23 14:51:42 +0300

    Add files via upload
    Week 1

---
+++ ex3.sh
@@ -0,0 +1,10 @@
+date
+sleep 3
+date
+
+
+/*
+done:
+Thu Aug 23 14:34:28 MSK 2018
+Thu Aug 23 14:34:31 MSK 2018
+*/
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
+
+/*
+ Done:
+ Daniils-MacBook-Air:week1 daniilshilintsev$ gcc main.c -o ex4
+ Daniils-MacBook-Air:week1 daniilshilintsev$ ./ex4
+ Hello World
+*/
